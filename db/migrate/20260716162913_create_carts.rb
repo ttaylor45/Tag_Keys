c0c7ac1dd8d078984d073c897523817059b1860e
ttaylor45 <ttaylor45@Academic.rrc.ca>
2026-07-16 11:45:51 -0500

    created a minor create carts model

---
+++ db/migrate/20260716162913_create_carts.rb
@@ -0,0 +1,14 @@
+class CreateCarts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :carts do |t|
+      t.references :user,
+                   null: true,
+                   foreign_key: true,
+                   index: false
+
+      t.timestamps
+    end
+
+    add_index :carts, :user_id, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_16_162252) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_16_162913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id", unique: true
+  end
 
   create_table "categories", force: :cascade do |t|
     t.boolean "active", default: true, null: false
@@ -98,6 +105,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_16_162252) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "game_genres", "games"
   add_foreign_key "game_genres", "genres"
   add_foreign_key "games", "categories"
