974f95f1ca97bfef147197f9bf66f352e82ba579
ttaylor45 <ttaylor45@Academic.rrc.ca>
2026-07-17 00:49:04 -0500

    Created and edited multple files regarding the storefront with devise and made a simple login and regristration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [ :username ]
+    )
+
+    devise_parameter_sanitizer.permit(
+      :account_update, keys: [ :username, :first_name, :last_name, :address_line, :address_line_2,
+    :city, :postal_code, :province_id ]
+    )
+  end
 end
