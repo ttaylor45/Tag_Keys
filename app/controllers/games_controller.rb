58a8e90e6d6402016b4680385379551a922937ec
ttaylor45 <ttaylor45@Academic.rrc.ca>
2026-07-22 14:36:30 -0500

    Worked on the first prototype for the search bar for feature 2.6. Updated the games controller to make sure the querys were getting from the sql, and made a simple search function in the games/index.html.erb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,8 +8,17 @@ def index
   if params[:category_id].present?
     @selected_category = Category.find_by(id: params[:category_id])
 
-    @games = @games.where(category_id: @selected_category.id) if @selected_category
+    if @selected_category
+      @games = @games.where(category_id: @selected_category.id)
+    end
   end
+
+if params[:query].present?
+  @query = params[:query].strip
+
+  @games = @games.where("games.title ILIKE :query OR games.description ILIKE :query",
+  query: "%#{Game.sanitize_sql_like(@query)}%")
+end
 
   @games = @games.order(:title)
 end
